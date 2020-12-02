799220fbdf6178b04def18563b0a6250a372d08f
mehmet <mehmetoktay2016@yandex.com>
2020-12-02 17:00:47 +0300

    8

--- app.py
+++ app.py
@@ -41,5 +41,13 @@ def predict():
 
 
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)
+    data_unseen = pd.DataFrame([data])
+    prediction = predict_model(model, data=data_unseen)
+    output = prediction.Label[0]
+    return jsonify(output)
+
+if __name__ == '__main__':
+    app.run(debug=True)
